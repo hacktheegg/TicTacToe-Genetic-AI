dc9d10cc8c4f0222a8038707752817e900f05460
hacktheegg <jarrahvanritten@gmail.com>
2023-04-14 22:58:28 +1000

    i think this works

--- Program.cs
+++ Program.cs
@@ -13,241 +13,74 @@ using Microsoft.VisualBasic;
 
 class Program
 {
-    static Random random = new Random();
-    static int populationSize = 50;
-    static int mutationRate = 10; // percentage
-    static int elitism = 10; // percentage
-    //static int[] target = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }; // target array to match
-
     static void Main(string[] args)
     {
-        // Create initial population //
-        // POPULATION A
-        int[][] populationA = new int[populationSize][];
-        for (int i = 0; i < populationSize; i++)
-        {
-            populationA[i] = GenerateRandomArray();
-        }
-        // POPULATION B
-        int[][] populationB = new int[populationSize][];
-        for (int i = 0; i < populationSize; i++)
-        {
-            populationB[i] = GenerateRandomArray();
-        }
+
+        int GenomeLength = 9;
+        int GenomeValueMax = 8;
 
 
+        /////////////////
+        // Innitialise //
+        /////////////////
+        int[] GenomeA = AI.GenerateRandomArray(GenomeLength, GenomeValueMax);
+        int[] GenomeB = AI.GenerateRandomArray(GenomeLength, GenomeValueMax);
 
-        // Evolution loop
-        int generation = 1;
+
         while (true)
         {
-            // Sort population by fitness //
+            bool[] winner = Game(GenomeA, GenomeB);
 
-            populationA = populationA.OrderBy(x => Fitness(x)).ToArray();
-            // POPULATION B
-            populationB = populationB.OrderBy(x => Fitness(x)).ToArray();
-
-
-
-            // Create new generation
-            int[][] newPopulation = new int[populationSize][];
-            int elitismCount = populationSize * elitism / 100;
-
-            // Elitism: copy top percentage of population to next generation
-            for (int i = 0; i < elitismCount; i++)
+            if (winner[1])
             {
-                newPopulation[i] = populationA[i].ToArray();
-            }
-
-            // Breed remaining population
-            for (int i = elitismCount; i < populationSize; i++)
-            {
-                int[] parent1 = SelectParent(populationA);
-                int[] parent2 = SelectParent(populationA);
-                int[] child = Breed(parent1, parent2);
-                newPopulation[i] = child;
-            }
-
-            // Mutate new generation
-            for (int i = elitismCount; i < populationSize; i++)
-            {
-                if (random.Next(0, 100) < mutationRate)
+                if (!winner[0])
                 {
-                    Mutate(newPopulation[i]);
-                }
-            }
-
-            populationA = newPopulation;
-            generation++;
-        }
-    }
-
-
-
-    static int[] GenerateRandomArray()
-    {
-        int[] array = Enumerable.Range(1, 9).ToArray();
-        for (int i = 0; i < array.Length; i++)
-        {
-            int randomIndex = random.Next(i, array.Length);
-            int temp = array[i];
-            array[i] = array[randomIndex];
-            array[randomIndex] = temp;
-        }
-        return array;
-    }
-
-    static int Fitness(int[] arrayA, int[] arrayB)
-    {
-        int fitness = 0;
-        for (int i = 0; i < array.Length; i++)
-        {
-            if (array[i] == target[i])
-            {
-                fitness++;
-            }
-        }
-
-        return fitness;
-    }
-
-    static int[] SelectParent(int[][] population)
-    {
-        int totalFitness = population.Sum(x => Fitness(x));
-        int randomFitness = random.Next(0, totalFitness);
-        int index = 0;
-        while (randomFitness > 0)
-        {
-            randomFitness -= Fitness(population[index]);
-            index++;
-        }
-
-        return index == 0 ? population[index] : population[index - 1];
-    }
-
-    static int[] Breed(int[] parent1, int[] parent2)
-    {
-        int[] child = new int[parent1.Length];
-        int start = random.Next(0, parent1.Length);
-        int end = random.Next(start, parent1.Length);
-        for (int i = start; i <= end; i++)
-        {
-            child[i] = parent1[i];
-        }
-        for (int i = 0; i < parent2.Length; i++)
-        {
-            if (!child.Contains(parent2[i]))
-            {
-                for (int j = 0; j < child.Length; j++)
+                    //int[] GenomeAMutated = AI.Mutate(GenomeA, GenomeValueMax);
+                    GenomeB = AI.Mutate(GenomeB, GenomeValueMax);
+                } else
                 {
-                    if (child[j] == 0)
-                    {
-                        child[j] = parent2[i];
-                        break;
-                    }
+                    //int[] GenomeBMutated = AI.Mutate(GenomeA, GenomeValueMax);
+                    GenomeA = AI.Mutate(GenomeA, GenomeValueMax);
                 }
-            }
-        }
-        return child;
-    }
-
-    static void Mutate(int[] array)
-    {
-        int index1 = random.Next(0, array.Length);
-        int index2 = random.Next(0, array.Length);
-        int temp = array[index1];
-        array[index1] = array[index2];
-        array[index2] = temp;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public static void Game()
-    {
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        /*var geneticAlgorithm = new GeneticAlgorithm(populationSize: 10, mutationRate: 0.01, crossoverRate: 0.8, elitismCount: 2);
-        var population = geneticAlgorithm.GeneratePopulation(chromosomeLength: 10, minValue: 0.0, maxValue: 1.0);
-
-        foreach (var candidate in population)
-        {
-            candidate.Fitness = CalculateFitness(candidate);
-        }
-
-        for (int i = 0; i < numGenerations; i++)
-        {
-            population = geneticAlgorithm.EvolvePopulation(population, minValue: 0.0, maxValue: 1.0);
-
-            foreach (var candidate in population)
+            } else
             {
-                candidate.Fitness = CalculateFitness(candidate);
+                GenomeB = AI.Mutate(GenomeB, GenomeValueMax);
+                GenomeA = AI.Mutate(GenomeA, GenomeValueMax);
             }
+            
         }
-
-        // Get the best candidate in the final population
-        var bestCandidate = population.OrderByDescending(c => c.Fitness).First();
-        Console.WriteLine($"Best candidate: {string.Join(", ", bestCandidate.Parameters)}");*/
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    }
 
 
 
+    public class AI
+    {
+        public static int[] GenerateRandomArray(int length, int maxInt)
+        {
+            Random random = new Random();
+
+            int[] array = new int[length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(0, maxInt);
+            }
+            return array;
+        }
+        public static int[] Mutate(int[] Genome, int GenomeValueMax)
+        {
+            Random random = new Random();
+
+            int randomIndex = random.Next(0, Genome.Length);
+            Genome[randomIndex] = random.Next(1, GenomeValueMax);
+
+            return Genome;
+        }
+    }
 
 
 
-
-        //var geneticAlgorithm = new GeneticAlgorithm(populationSize: 10, mutationRate: 0.01, crossoverRate: 0.8, elitismCount: 2);
-        //var population = geneticAlgorithm.GeneratePopulation(chromosomeLength: 10, minValue: 0.0, maxValue: 1.0);
-
-
+    public static bool[] Game(int[] GenomeA, int[] GenomeB)
+    {
         int[][] Board = new int[3][];
         Board[0] = new int[3] { 0, 0, 0 };
         Board[1] = new int[3] { 0, 0, 0 };
@@ -257,56 +90,56 @@ class Program
         // false = X
         //  true = O
         bool turn = false;
-        bool failedTurn = false;
+        int TurnNo = 0;
 
         int tempInt;
 
 
-        while (!(CheckState(Board).Item1 || !Board.SelectMany(x => x).Any(x => x == 0)))
+        while (!(CheckState(Board) || !Board.SelectMany(x => x).Any(x => x == 0)))
         {
-        failedTurn:
+            Thread.Sleep(250);
+
             Console.Clear();
             PrintTicTacToe(Board);
 
-            if (failedTurn)
+            int CurrentMove;
+
+            if (!turn)
             {
-                Console.WriteLine("INVALID INPUT");
+                CurrentMove = GenomeA[TurnNo];
+            } else
+            {
+                CurrentMove = GenomeB[TurnNo];
             }
 
-            Console.WriteLine("Player \"" + XY(turn) + "\", it is currently your turn");
+            failedTurn:
 
-            Console.Write("X Axis: ");
-            string XAxisString = Console.ReadKey(false).KeyChar.ToString().ToLower();
-            if (!int.TryParse(XAxisString, out tempInt))
-            {
-                failedTurn = true;
-                goto failedTurn;
-            }
-            else if (int.Parse(XAxisString) < 1 || int.Parse(XAxisString) > 3)
-            {
-                failedTurn = true;
-                goto failedTurn;
-            }
-            int XAxis = int.Parse(XAxisString) - 1;
 
-            Console.Write("\nY Axis: ");
-            string YAxisString = Console.ReadKey(false).KeyChar.ToString().ToLower();
-            if (!int.TryParse(YAxisString, out tempInt))
+            int XAxis;
+            int YAxis;
+
+            
+
+            if (!turn)
             {
-                failedTurn = true;
-                goto failedTurn;
-            }
-            else if (int.Parse(YAxisString) < 1 || int.Parse(YAxisString) > 3)
+                XAxis = CurrentMove % 3;
+                YAxis = Convert.ToInt32(Math.Floor((float)(CurrentMove / 3)));
+            } else
             {
-                failedTurn = true;
-                goto failedTurn;
+                XAxis = CurrentMove % 3;
+                YAxis = Convert.ToInt32(Math.Floor((float)(CurrentMove / 3)));
             }
-            int YAxis = int.Parse(YAxisString) - 1;
+
 
 
             if (Board[YAxis][XAxis] != 0)
             {
-                failedTurn = true;
+                CurrentMove++;
+
+                if (CurrentMove > 8)
+                {
+                    CurrentMove = 0;
+                }
                 goto failedTurn;
             }
 
@@ -314,15 +147,15 @@ class Program
             if (!turn)
             {
                 Board[YAxis][XAxis] = 1;
-            }
-            else
+            } else
             {
                 Board[YAxis][XAxis] = 2;
             }
 
 
             turn = !turn;
-            failedTurn = false;
+
+            TurnNo++;
         }
         //int tempInt;
 
@@ -331,7 +164,7 @@ class Program
 
         Console.Clear();
         PrintTicTacToe(Board);
-        if (CheckState(Board).Item1)
+        if (CheckState(Board))
         {
             Console.WriteLine("Congrats \"" + XY(!turn) + "\", You won!");
         }
@@ -339,19 +172,9 @@ class Program
         {
             Console.WriteLine("It seems it is a tie, Good game for both sides.");
         }
-        Console.ReadKey(true);
-    }
-
-
-    public static double EvaluateFitness(int[][] Board)
-    {
-        if (CheckState(Board).Item2 == 1)
-        {
-            return 1.0;
-        } else
-        {
-            return 0.0;
-        }
+        //Console.ReadKey(true);
+        bool[] returnBool = { !turn, CheckState(Board) };
+        return returnBool;
     }
 
 
@@ -368,53 +191,52 @@ class Program
     }
 
 
-
-    public static Tuple<bool, int> CheckState(int[][] Board)
+    public static bool CheckState(int[][] Board)
     {
-        if (CheckStateVertical(Board).Item1)
+        if (CheckStateVertical(Board))
         {
             return CheckStateVertical(Board);
-        } else if (CheckStateHorizontal(Board).Item1)
+        } else if (CheckStateHorizontal(Board))
         {
             return CheckStateHorizontal(Board);
-        } else if (CheckStateDiagonal(Board).Item1)
+        } else if (CheckStateDiagonal(Board))
         {
             return CheckStateDiagonal(Board);
         }
-        return Tuple.Create(false, 0);
+        return false;
     }
-    public static Tuple<bool, int> CheckStateVertical(int[][] Board)
+    public static bool CheckStateVertical(int[][] Board)
     {
         for (int iterate = 0; iterate <= 2; iterate++)
         {
             if (Board[iterate][0] == Board[iterate][1] && Board[iterate][1] == Board[iterate][2] && Board[iterate][0] != 0)
             {
-                return Tuple.Create(true, Board[iterate][0]);
+                return true;
             }
         }
-        return Tuple.Create(false, 0);
+        return false;
     }
-    public static Tuple<bool, int> CheckStateHorizontal(int[][] Board)
+    public static bool CheckStateHorizontal(int[][] Board)
     {
         for (int iterate = 0; iterate <= 2; iterate++)
         {
             if (Board[0][iterate] == Board[1][iterate] && Board[1][iterate] == Board[2][iterate] && Board[0][iterate] != 0)
             {
-                return Tuple.Create(true, Board[0][iterate]);
+                return true;
             }
         }
-        return Tuple.Create(false, 0);
+        return false;
     }
-    public static Tuple<bool, int> CheckStateDiagonal(int[][] Board)
+    public static bool CheckStateDiagonal(int[][] Board)
     {
         if (Board[0][0] == Board[1][1] && Board[1][1] == Board[2][2] && Board[0][0] != 0)
         {
-            return Tuple.Create(true, Board[0][0]);
+            return true;
         } else if (Board[2][0] == Board[1][1] && Board[1][1] == Board[0][2] && Board[2][0] != 0)
         {
-            return Tuple.Create(true, Board[2][0]);
+            return true;
         }
-        return Tuple.Create(false, 0);
+        return false;
     }
 
 
